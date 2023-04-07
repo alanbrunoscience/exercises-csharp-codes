0eec0327050ff32cd6c85522164fab295119895a
Alan Bruno <alanbrunomath@gmail.com>
2023-04-07 19:43:40 -0300

    Create a repository files

---
+++ 01_Programming Logic/Section01_DataTypes_Operators_OutputsCommands/Exerc01_LogicalOperators/Exerc01_LogicalOperators/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Exerc01_LogicalOperators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variables Declaration
+            bool A = true;
+            bool B = false;
+
+            // Logical Results
+            Console.WriteLine("a) !A && B || A && !B = " + (!A && B || A && !B));
+            Console.WriteLine("b) !(!(A || B) && (A || B)) = " + (!(!(A || B) && (A || B))));
+            Console.WriteLine("c) A || B && !A || !B = " + (A || B && !A || !B));
+            Console.WriteLine("d) (A || B) && (!A || B) = " + ((A || B) && (!A || B)));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 01_Programming Logic/Section01_DataTypes_Operators_OutputsCommands/Exerc03_PersonalInformation/Exerc03_PersonalInformation/Exerc03_PersonalInformation.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+
+namespace Exerc03_PersonalInformation
+{
+    internal class Exerc03_PersonalInformation
+    {
+        static void Main(String[] args)
+        {
+            // Faça um algoritmo para imprimir a sua idade, o seu peso e o seu sexo.
+            // Indicar o sexo como sendo: f – feminino ou m – masculino.
+
+            // Variable Declaration
+            int age = 26;
+            double weight = 87.7;
+            char gender = 'M';
+
+            // Data Outputs
+
+        }
+    }
+}
