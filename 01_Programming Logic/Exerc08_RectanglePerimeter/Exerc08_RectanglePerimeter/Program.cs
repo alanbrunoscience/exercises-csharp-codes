72b2c2a7d29ee084d187bf8dcba17c02d6f8a1fe
Alan Bruno <alanbrunomath@gmail.com>
2023-04-07 21:51:37 -0300

    Create a new project

---
+++ 01_Programming Logic/Exerc08_RectanglePerimeter/Exerc08_RectanglePerimeter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+namespace Exerc08_RectanglePerimeter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variables Declaration
+            double rectangleBase = 10.0;
+            double rectangleHeight = 35.0;
+
+            // Rectangle Perimeter
+            double perimeter = 2 * (rectangleBase + rectangleHeight);
+
+            // Data Output
+            Console.WriteLine("The rectangle perimeter is: " + perimeter.ToString("F2", CultureInfo.InvariantCulture) + " cm." );
+
+            Console.ReadLine();
+        }
+    }
+}
