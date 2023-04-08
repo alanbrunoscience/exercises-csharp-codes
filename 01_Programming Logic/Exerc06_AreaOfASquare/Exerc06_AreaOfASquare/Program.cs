b6a2fd6b3a387e6e870620864ff8abc7d56d74a8
Alan Bruno <alanbrunomath@gmail.com>
2023-04-07 21:26:40 -0300

    Create a new project

---
+++ 01_Programming Logic/Exerc06_AreaOfASquare/Exerc06_AreaOfASquare/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+namespace Exerc06_AreaOfASquare
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variables Declaration
+            double l = 350.0;
+            double area = Math.Pow(l, 2);
+
+            // Data Output
+            Console.WriteLine("The area of square is: " + area.ToString("F2", CultureInfo.InvariantCulture) + " m².");
+        }
+    }
+}
