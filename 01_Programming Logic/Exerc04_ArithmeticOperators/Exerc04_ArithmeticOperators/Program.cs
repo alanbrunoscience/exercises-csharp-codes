45f76401e71b639467a6190b941552d39e633439
Alan Bruno <alanbrunomath@gmail.com>
2023-04-07 21:20:07 -0300

    Update code comment

---
+++ 01_Programming Logic/Exerc04_ArithmeticOperators/Exerc04_ArithmeticOperators/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Exerc04_ArithmeticOperators
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+            // Faça um algoritmo para apresentar o resultado lógico das seguintes expressões:
+            // ((120 - 30) = (3 x 30))
+			// (não((20 % 4) = 1) ou(9 != 9))
+			// ((5 % 2 / 2) > 3)
+			// (a = A)
+
+			// Data Output
+			Console.WriteLine("> ((120 - 30) = (3 x 30)): " + ((120 - 30) == (3 * 30)));
+			Console.WriteLine("> (!((20 % 4) = 1) ou (9 != 9)): " + (!((20 % 4) == 1) || (9 != 9)));
+            Console.WriteLine("> ((5 % 2 / 2) > 3)): " + ((5 % 2 / 2) > 3));
+			Console.WriteLine("> (a = A): " + ('a' == 'A'));
+
+			Console.ReadLine();
+        }
+	}
+}
---
+++ 01_Programming Logic/Exerc05_ArithmeticAverage/Exerc05_ArithmeticAverage/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+
+namespace Exerc05_ArithmeticAverage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variable Declaration
+            double n1 = 8.0;
+            double n2 = 7.5;
+            double n3 = 4.5;
+            double n4 = 9.0;
+            double average = 0;
+
+            // Data Output
+            Console.WriteLine("====== Arithmetic Average ======");
+            average = (n1 + n2 + n3 + n4) / 4;
+            Console.WriteLine("> The average is: " + average.ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.ReadLine();
+        }
+    }
+}
