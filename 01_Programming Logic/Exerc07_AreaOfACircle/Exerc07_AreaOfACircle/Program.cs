dafa5ea8dc627ebb16844869dab4d73120db39c0
Alan Bruno <alanbrunomath@gmail.com>
2023-04-07 21:41:36 -0300

    Create a new project

---
+++ 01_Programming Logic/Exerc07_AreaOfACircle/Exerc07_AreaOfACircle/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+namespace Exerc07_AreaOfACircle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variables Declaration
+            double pi = Math.PI;
+            double radius = 5.0;
+
+            // Area of a circle
+            double circleArea = (pi * Math.Pow(radius, 2));
+
+            // Data Output
+            Console.WriteLine("The area of circle is: " + circleArea.ToString("F2", CultureInfo.InvariantCulture) + " cm².");
+
+            Console.ReadLine();
+        }
+    }
+}
