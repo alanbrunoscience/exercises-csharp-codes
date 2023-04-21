cfab6e0d678c19f79f1ee459a388b82e385adf39
Alan Bruno <alanbrunomath@gmail.com>
2023-04-21 15:20:25 -0300

    Solve more exercises

---
+++ Programming_Logic/DataInputCommands/Exerc07_ProductOfNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exerc07_ProductOfNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variables Declaration
+            int n1, n2, prod;
+
+            // Data Input
+            Console.WriteLine("Enter the first number:");
+            n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second number:");
+            n2 = int.Parse(Console.ReadLine());
+
+            // Product Of Numbers
+            prod = n1 * n2;
+
+            // Data Output
+            Console.WriteLine();
+            Console.WriteLine(String.Format("{0} x {1} = {2}", n1, n2, prod));
+        }
+    }
+}
