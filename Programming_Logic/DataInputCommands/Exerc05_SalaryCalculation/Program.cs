4f79bdc3d63cbb0ff2d4aa40638c77abefbae667
Alan Bruno <alanbrunomath@gmail.com>
2023-04-20 22:32:44 -0300

    Update repository

---
+++ Programming_Logic/DataInputCommands/Exerc05_SalaryCalculation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exerc05_SalaryCalculation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Data Input
+            Console.WriteLine("Inform your salary, please:");
+            double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            // Salary Calculation
+            double newSalary = salary * 1.25;
+
+            // Data Output
+            Console.WriteLine();
+            Console.WriteLine("Your new salary is US$ {0}.", newSalary.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ Programming_Logic/DataInputCommands/Exerc06_TriangleArea/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exerc06_TriangleArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Data Input
+            Console.WriteLine("Enter the value of the triangle base (cm):");
+            double triangleBase = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Enter the value of the triangle height (cm):");
+            double triangleHeight = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            // Area Calculation
+            double area = ((triangleBase * triangleHeight) / 2);
+
+            // Data Output
+            Console.WriteLine();
+            Console.WriteLine(String.Format("The triangle area is {0} cm².", area.ToString("F2", CultureInfo.InvariantCulture)));
+        }
+    }
+}
